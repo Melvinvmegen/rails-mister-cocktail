f93bdc2d035bfc78e7ac55279d4fd1c848c3726d
melvin van megen <Melvin.vmegen@gmail.com>
2019-11-08 13:57:46 +0100

    02-improved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_151521) do
+ActiveRecord::Schema.define(version: 2019_11_08_111345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,18 +22,17 @@ ActiveRecord::Schema.define(version: 2019_11_07_151521) do
   end
 
   create_table "doses", force: :cascade do |t|
+    t.string "description"
     t.bigint "cocktail_id"
     t.bigint "ingredient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "description"
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
-    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
